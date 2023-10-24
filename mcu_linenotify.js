325faddc835477390a2c345556ab0156e17d9dee
404background <404background@gmail.com>
2023-10-24 23:20:22 +0900

    エラー："cannot coerce undefined to object"への対応
    credentialsだとトークンが保存されない可能性を考えて変更

--- mcu_linenotify.js
+++ mcu_linenotify.js
@@ -2,19 +2,14 @@ module.exports = function(RED) {
     function McuLineNotifyNode(config) {
         var node = this;
         RED.nodes.createNode(this,config);
-        const LINE_TOKEN = node.credentials.AccessToken;
-        const token = {
-            'Authorization': `Bearer ${LINE_TOKEN}`
-        }
+        let LINE_TOKEN = this.AccessToken;
         node.on('input', function(msg) {
-            msg.headers=token;
+            msg.headers={
+                'Authorization': `Bearer ${LINE_TOKEN}`
+            };
             msg.url=`https://notify-api.line.me/api/notify?message=${encodeURI(msg.payload)}`;
             node.send(msg);
         });
     }
-    RED.nodes.registerType("mcu_linenotify", McuLineNotifyNode, {
-        credentials: {
-            AccessToken: {type:"password"}
-        }
-    });
+    RED.nodes.registerType("mcu_linenotify", McuLineNotifyNode);
 }
