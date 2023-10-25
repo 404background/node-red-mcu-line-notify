e497cd7541844294ea0850604c237c9b13808f59
404background <404background@gmail.com>
2023-10-25 10:38:48 +0900

    AccessTokenが渡せていなかったので修正

--- mcu_linenotify.js
+++ mcu_linenotify.js
@@ -2,7 +2,8 @@ module.exports = function(RED) {
     function mcu_linenotify(config) {
         var node = this;
         RED.nodes.createNode(this,config);
-        let LINE_TOKEN = this.AccessToken;
+        this.AccessToken = config.AccessToken;
+        let LINE_TOKEN = node.AccessToken;
         node.on('input', function(msg) {
             msg.headers={
                 'Authorization': `Bearer ${LINE_TOKEN}`
